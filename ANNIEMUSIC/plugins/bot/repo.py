5d90110ea4baacad80cf9499ea343bbd3db40f2d
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-24 13:44:16 +0530

    Update repo.py

--- ANNIEMUSIC/plugins/bot/repo.py
+++ ANNIEMUSIC/plugins/bot/repo.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from VIPMUSIC import app
+from ANNIEMUSIC import app
 import httpx 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
